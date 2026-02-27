9acfb9e18a1cb558eb1a102801b3acd6cc9f3d10
Renato Dehnhardt <renatotkd@gmail.com>
2026-02-27 10:36:30 +0100

    fix upload file in chats with selected agent

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
+        $middleware->encryptCookies(except: ['appearance']);
+        $middleware->trustProxies(at: '*');
 
         $middleware->web(append: [
             HandleAppearance::class,
